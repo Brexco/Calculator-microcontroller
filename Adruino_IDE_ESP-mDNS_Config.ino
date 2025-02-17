8c8b90c1890f1810f87d7d8e46e0faa4973e2df6
Brexco <jan.wygoda@iu-study.org>
2025-02-17 10:09:23 +0100

    Update Adruino_IDE_ESP-mDNS_Config.ino

--- Adruino_IDE_ESP-mDNS_Config.ino
+++ Adruino_IDE_ESP-mDNS_Config.ino
@@ -1,9 +1,9 @@
 /**
+* @author Jan Wygoda
+* @version 01.00.05
+* @date 12.02.2025
 * @file ESP8266_Calculator_Server.cpp
 * @brief Einfache Webserver-Anwendung zur Berechnung mathematischer Ausdrücke auf einem ESP8266.
-* @author Jan Wygoda
-* @date 2025-02-12
-* @version 01.00.04
 */
 
 //////////////////////////////
@@ -25,6 +25,65 @@ const int iLedProcessing = D7; ///< Gelbe LED: Verarbeitung
 ESP8266WebServer server(80); // HTTP-Server auf Port 80
 
 /**
+* @brief Einrichtung der Hardware und Netzwerkverbindung.
+*/
+void setup() {
+  Serial.begin(115200);
+
+  setupPins();
+  connectToWiFi();
+  startMDNS();
+  startServer();
+}
+
+/**
+* @brief Konfiguriert die Pins.
+*/
+void setupPins() {
+  pinMode(iLedReset, OUTPUT);
+  pinMode(iLedServer, OUTPUT);
+  pinMode(iLedProcessing, OUTPUT);
+
+  digitalWrite(iLedReset, HIGH);
+  digitalWrite(iLedServer, LOW);
+  digitalWrite(iLedProcessing, LOW);
+}
+
+/**
+* @brief Stellt die Verbindung zum WiFi her.
+*/
+void connectToWiFi() {
+  WiFi.begin(cSsid, cPassword);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to WiFi");
+}
+
+/**
+* @brief Startet mDNS.
+*/
+void startMDNS() {
+  if (MDNS.begin("calculator")) {
+    Serial.println("mDNS responder started");
+  }
+
+  MDNS.addService("http", "tcp", 80);
+}
+
+/**
+* @brief Startet den HTTP-Server und registriert die Endpunkte.
+*/
+void startServer() {
+  server.on("/calculate", handleCalculate);
+  server.begin();
+  Serial.println("HTTP-Server gestartet");
+  digitalWrite(iLedServer, HIGH);
+}
+
+/**
 * @brief Verarbeitung einer mathematischen Berechnung.
 * @details Diese Funktion empfängt einen mathematischen Ausdruck als HTTP-Parameter,
 *          führt die Berechnung durch und sendet das Ergebnis als Antwort.
@@ -33,44 +92,13 @@ void handleCalculate() {
   digitalWrite(iLedProcessing, HIGH); // Verarbeitung starten (LED an)
 
   if (server.hasArg("expression")) {
-    String sExpression = server.arg("expression");
+    String expression = server.arg("expression");
+    float result = performCalculation(expression);
     
-    // Operator und Zahlen ermitteln
-    char op = 0;
-    int opIndex = -1;
-
-    if ((opIndex = sExpression.indexOf('+')) > 0) op = '+';
-    else if ((opIndex = sExpression.indexOf('-')) > 0) op = '-';
-    else if ((opIndex = sExpression.indexOf('*')) > 0) op = '*';
-    else if ((opIndex = sExpression.indexOf('/')) > 0) op = '/';
-
-    if (op != 0) {
-      int num1 = sExpression.substring(0, opIndex).toInt();
-      int num2 = sExpression.substring(opIndex + 1).toInt();
-      float result;
-
-      switch (op) {
-        case '+': result = num1 + num2; break;
-        case '-': result = num1 - num2; break;
-        case '*': result = num1 * num2; break;
-        case '/': 
-          if (num2 != 0) {
-            result = (float)num1 / num2; 
-          } else {
-            server.send(400, "text/plain", "Division by 0 not allowed");
-            digitalWrite(iLedProcessing, LOW);
-            return;
-          }
-          break;
-        default:
-          server.send(400, "text/plain", "Invalid operator");
-          digitalWrite(iLedProcessing, LOW);
-          return;
-      }
-
-      server.send(200, "text/plain", String(result));
+    if (isnan(result)) {
+      server.send(400, "text/plain", "Invalid expression or operator.");
     } else {
-      server.send(400, "text/plain", "Invalid expression. Supported operators: +, -, *, /");
+      server.send(200, "text/plain", String(result));
     }
   } else {
     server.send(400, "text/plain", "No expression given");
@@ -80,37 +108,55 @@ void handleCalculate() {
 }
 
 /**
-* @brief Einrichtung der Hardware und Netzwerkverbindung.
+* @brief Führt die mathematische Berechnung durch.
+* @param expression Der mathematische Ausdruck als String.
+* @return Das Ergebnis der Berechnung oder NaN, falls ungültig.
 */
-void setup() {
-  Serial.begin(115200);
+float performCalculation(const String& expression) {
+  char op = 0;
+  int opIndex = findOperator(expression, op);
 
-  pinMode(iLedReset, OUTPUT);
-  pinMode(iLedServer, OUTPUT);
-  pinMode(iLedProcessing, OUTPUT);
-
-  digitalWrite(iLedReset, HIGH);
-  digitalWrite(iLedServer, LOW);
-  digitalWrite(iLedProcessing, LOW);
-
-  WiFi.begin(cSsid, cPassword);
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nConnected to WiFi");
-
-  if (MDNS.begin("calculator")) {
-    Serial.println("mDNS responder started");
+  if (opIndex == -1) {
+    return NAN; // Ungültiger Ausdruck
   }
 
-  server.on("/calculate", handleCalculate);
-  server.begin();
-  Serial.println("HTTP-Server gestartet");
+  int num1 = expression.substring(0, opIndex).toInt();
+  int num2 = expression.substring(opIndex + 1).toInt();
 
-  MDNS.addService("http", "tcp", 80);
-  digitalWrite(iLedServer, HIGH);
+  switch (op) {
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case '*': return num1 * num2;
+    case '/': 
+      if (num2 != 0) {
+        return (float)num1 / num2;
+      } else {
+        return NAN; // Division durch 0
+      }
+    default: return NAN; // Ungültiger Operator
+  }
+}
+
+/**
+* @brief Findet den Operator im Ausdruck.
+* @param expression Der mathematische Ausdruck als String.
+* @param op Der Operator, der gefunden wird.
+* @return Der Index des Operators oder -1, wenn kein gültiger Operator gefunden wurde.
+*/
+int findOperator(const String& expression, char& op) {
+  int opIndex = expression.indexOf('+');
+  if (opIndex > 0) { op = '+'; return opIndex; }
+  
+  opIndex = expression.indexOf('-');
+  if (opIndex > 0) { op = '-'; return opIndex; }
+  
+  opIndex = expression.indexOf('*');
+  if (opIndex > 0) { op = '*'; return opIndex; }
+  
+  opIndex = expression.indexOf('/');
+  if (opIndex > 0) { op = '/'; return opIndex; }
+  
+  return -1; // Kein Operator gefunden
 }
 
 /**
